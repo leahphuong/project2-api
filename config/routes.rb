3f90c4b9b3aa7a51b9342700b021db9ef3497be8
leahphuong <haphuong.ng@gmail.com>
2015-11-10 14:47:02 -0500

    update routes with profiles
    add profiles resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
 
 
-  get 'profiles/index'
-
-  get 'profiles/show'
-
-  get 'profiles/create'
-
-  get 'profiles/update'
-
-  get 'profiles/destroy'
-
   #becoming a user
   post '/register' => 'auth#register'
   # patch '/confirm' => 'auth#confirm'
@@ -25,5 +15,7 @@ Rails.application.routes.draw do
   resources :items, except: [:new, :edit]
 
   resources :collections, except: [:new, :edit]
+
+  resources :profiles, except: [:new, :edit]
 end
 
