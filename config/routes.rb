93796efd5d53657b3a336f7312b662af12f2c0cb
leahphuong <haphuong.ng@gmail.com>
2015-11-13 00:05:10 -0500

    update profiles routes
    changed from using the resources: profiles to:
    get "/profiles", to: "profiles#show"
      post "/profiles", to: "profiles#create"
      patch "/profiles", to: "profiles#update"
      delete "/profiles", to: "profiles#destroy"
    because each user only has one profile, no profile id needed.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,12 @@ Rails.application.routes.draw do
   resources :collections, except: [:new, :edit] do
     resources :items, except: [:new, :edit]
   end
+  # don't need the profiles id
+  # resources :profiles, except: [:new, :edit]
+  get "/profiles", to: "profiles#show"
+  post "/profiles", to: "profiles#create"
+  patch "/profiles", to: "profiles#update"
+  delete "/profiles", to: "profiles#destroy"
 
-  resources :profiles, except: [:new, :edit]
 end
 
